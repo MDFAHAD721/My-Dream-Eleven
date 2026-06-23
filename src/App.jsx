62e6f3986454172454dde58b24876543de0b5d8c
Sheikh Fahad <sheikhfahad721@gmail.com>
2026-06-23 13:55:16 +0600

    added navbar of my first react website

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import navImg from "./assets/logo.png";
+import coinImg from "./assets/Currency.png";
+
+function App() {
+  return (
+    <>
+      <div className="navbar w-11/12 mx-auto pt-5  ">
+        <div className="flex-1">
+          <a className="text-xl">
+            <img src={navImg} alt="logo" className="w-15 mr-2" />
+          </a>
+        </div>
+        <div className="flex items-center gap-6">
+          <div className="flex gap-6">
+            <a href="">Home</a>
+            <a href="">Fixture</a>
+            <a href="">Teams</a>
+            <a href="">Schedules</a>
+          </div>
+
+          <button className="flex items-center border border-gray-300 rounded-lg px-4 py-2">
+            <span className="font-bold">0</span>
+            <span className="mr-2 ml-1 font-bold">Coin</span>
+            <img className="w-6 h-6" src={coinImg} alt="coin" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
